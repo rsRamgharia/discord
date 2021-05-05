73d4ac530fd9dcb22922f6de67381c15523608a8
rsRamgharia <singhrobin1238014@gmail.com>
2021-05-05 22:38:19 +0530

    Setup discord clone

---
+++ src/components/Sidebar.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Sidebar = () => {
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <h3>Full Stack Developer</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
